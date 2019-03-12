3290509abbc5526342cf34ead89ce1b5242697cb
Unknown <ReksetoAjx@gmail.com>
2019-03-13 00:45:10 +0100

    Prettier

--- backend/App.js
+++ backend/App.js
@@ -1,24 +1,23 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const db = require('./database');
-const callDir = require('call-dir');
-const path = require('path');
+const Koa = require("koa");
+const Router = require("koa-router");
+const db = require("./database");
+const callDir = require("call-dir");
+const path = require("path");
 
 async function initServer(config) {
-const app = new Koa();
-const database = await db.initDatabase(config,console);
-const router = new Router();
-const logger = console;
+  const app = new Koa();
+  const database = await db.initDatabase(config, console);
+  const router = new Router();
+  const logger = console;
 
-const routes = path.resolve(__dirname, "./routes");
-callDir.loadAll(routes, fpath => require(fpath)(router,{database,logger}));
+  const routes = path.resolve(__dirname, "./routes");
+  callDir.loadAll(routes, fpath =>
+    require(fpath)(router, { database, logger })
+  );
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
-
-app.listen(config.PORT);
+  app.use(router.routes()).use(router.allowedMethods());
+  console.log("App listens on" + config.PORT);
+  app.listen(config.PORT);
 }
-
 
 initServer(process.env);
--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -1,11 +1,15 @@
-module.exports = function(router,{databae,logger}) {
+module.exports = function(router, { databae, logger }) {
+  router.get("/test/", (ctx, next) => {
+    console.log("I just got request");
+    ctx.body = {
+      success: true
+    };
+  });
 
-router.get('/test/', (ctx,next) => {
-
- ctx.body = {
-  success:true
- };
-
-});
-
-}
+  router.get("/", (ctx, next) => {
+    console.log("I just got request");
+    ctx.body = {
+      success: true
+    };
+  });
+};
