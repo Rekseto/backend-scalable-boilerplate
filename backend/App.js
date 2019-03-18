53bf47aaa5b6a9b0c43e9c8574120d715522b949
Rekseto <ReksetoAjx@gmail.com>
2019-03-18 20:45:08 +0100

    Winston logging

--- backend/App.js
+++ backend/App.js
@@ -3,49 +3,53 @@ const Router = require("koa-router");
 const db = require("./database");
 const callDir = require("call-dir");
 const path = require("path");
-const winston = require("winston");
-
+const { createLogger, format, transports } = require("winston");
 const levels = {
-  log: 0,
-  error: 1,
-  critical: 2,
-  info: 3
+  info: 0, // harmless actions
+  notify: 1, // potential dangerous actions
+  error: 2, // erros
+  critical: 3 // criticals
 };
 
-
-
-const winstonLogger = winston.createLogger({
+const winstonLogger = createLogger({
+  format: format.combine(format.timestamp(), format.json()),
   transports: [
-    new winston.transports.Console({ level: 'log' }),
-    new winston.transports.File({
-      filename: 'info.log',
-      level: 'info'
+    new transports.Console({ level: "notify" }),
+    new transports.Console({ level: "error" }),
+    new transports.File({
+      filename: "/var/project/logs/notify.log",
+      level: "notify"
     }),
-    new winston.transports.File({
-      filename: 'errors.log',
-      level: 'error'
+    new transports.File({
+      filename: "/var/project/logs/info.log",
+      level: "info"
+    }),
+    new transports.File({
+      filename: "/var/project/logs/errors.log",
+      level: "error"
     })
   ],
   levels
-})
-
+});
 
 async function initServer(config) {
-  const app = new Koa();
-  const database = await db.initDatabase(config, console);
-  const router = new Router();
   const logger = winstonLogger;
+  const router = new Router();
+  const app = new Koa();
+  try {
+    const database = await db.initDatabase(config, console);
+    logger.notify(`BACKEND:${config.SERVER_ID} started`);
 
-  logger.log('log', `BACKEND:${config.SERVER_ID} started`);
+    const routes = path.resolve(__dirname, "./routes");
+    callDir.loadAll(routes, fpath =>
+      require(fpath)(router, { database, logger })
+    );
 
-  const routes = path.resolve(__dirname, "./routes");
-  callDir.loadAll(routes, fpath =>
-    require(fpath)(router, { database, logger })
-  );
-
-  app.use(router.routes()).use(router.allowedMethods());
-  console.log("App listens on" + config.PORT);
-  app.listen(config.PORT);
+    app.use(router.routes()).use(router.allowedMethods());
+    app.listen(config.PORT);
+  } catch (error) {
+    logger.critical(error.message);
+  }
 }
 
 initServer(process.env);
--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -1,13 +1,5 @@
 module.exports = function(router, { databae, logger }) {
-  router.get("/test/", (ctx, next) => {
-    console.log("I just got request");
-    ctx.body = {
-      success: true
-    };
-  });
-
   router.get("/", (ctx, next) => {
-    console.log("I just got request");
     ctx.body = {
       success: true
     };
